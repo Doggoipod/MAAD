ba47f86b4c59a2025281cdd5d97a9c32a31d3a9d
Nicolack <105887686+Doggoipod@users.noreply.github.com>
2024-10-08 10:43:28 -0300

    \x

---
+++ main.js
@@ -0,0 +1,9 @@
+function motraResposta (cartao__conteudo){
+    const reposta = cartao__conteudo.querySelector('cartao__conteudo');
+    if(resposta.style == 'none'){
+        resposta.style == 'block'
+    }   
+    else{
+        resposta.style == 'none'
+    }
+}
