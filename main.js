8188bc8ed1a77a6867bd561ebe6e8581e15b82dc
Nicolack <105887686+Doggoipod@users.noreply.github.com>
2024-10-24 10:41:55 -0300

    xzc

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 function motraResposta (card){
-    const reposta = card.querySelector('cartao__conteudo__resposta');
+    const resposta = card.querySelector('cartao__conteudo__resposta');
 
-    if(resposta.style.display == 'none'){
-        resposta.style.display == 'block'
+    if(resposta.style.display === 'none'){
+        resposta.style.display = 'block'
     }   
     else{
-        resposta.style.display == 'none'
+        resposta.style.display = 'none'
     }
 }
